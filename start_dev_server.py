6c9119c4bc7107e3a0f2a99b0fb59f3c9e9728ab
Sinha-Ujjawal <https://github.com/Sinha-Ujjawal>
2022-06-08 19:00:53 +0530

    Changing timedelta to 3 seconds for frequent ping. This seemed to work fine

--- start_dev_server.py
+++ start_dev_server.py
@@ -3,7 +3,7 @@ from prefect import Flow, task
 import subprocess
 
 
-@task(retry_delay=timedelta(seconds=30), max_retries=3, log_stdout=True)
+@task(retry_delay=timedelta(seconds=3), max_retries=10, log_stdout=True)
 def start_actix_web_server():
     return_code = subprocess.call("cargo run", shell=True)
     assert return_code != 3, "Rust process aborted!"
