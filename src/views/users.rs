129d7e187aef0d4a3dc5c5dbd5facf9a079fc27b
Sinha-Ujjawal <https://github.com/Sinha-Ujjawal>
2022-06-12 01:33:28 +0530

    Blocking db interactions (for both redis and postgres)

--- src/views/users.rs
+++ src/views/users.rs
@@ -47,11 +47,15 @@ async fn signup(
 ) -> actix_web::Result<web::Json<UserData>, MyError> {
     let conn = db.get().map_err(|_| MyError::InternalServerError)?;
 
-    let user = add_user(
-        conn,
-        input_user.username.clone(),
-        input_user.password.clone(),
-    )
+    let user = web::block(move || {
+        add_user(
+            conn,
+            input_user.username.clone(),
+            input_user.password.clone(),
+        )
+    })
+    .await
+    .map_err(|_| MyError::InternalServerError)?
     .map_err(|err| match err {
         DatabaseError(DatabaseErrorKind::UniqueViolation, _) => MyError::UserAlreadyExists,
         _ => MyError::DieselError(err),
@@ -77,13 +81,18 @@ async fn login(
     input_user: web::Json<LogInInput>,
     auth_mgr: web::Data<AuthManager>,
 ) -> actix_web::Result<web::Json<Token>, MyError> {
+    let password = input_user.password.clone();
+
     let conn = db.get().map_err(|_| MyError::InternalServerError)?;
-    let user = get_user_by_username(conn, input_user.username.clone())
+    let user = web::block(move || get_user_by_username(conn, input_user.username.clone()))
+        .await
+        .map_err(|_| MyError::InternalServerError)?
         .map_err(|_| MyError::UserDoesNotExists)?;
 
-    if validate_password(input_user.password.clone(), user.password_hash) {
-        let token = auth_mgr
-            .create_token(user.id)
+    if validate_password(password.clone(), user.password_hash) {
+        let token = web::block(move || auth_mgr.create_token(user.id))
+            .await
+            .map_err(|_| MyError::InternalServerError)?
             .ok_or(MyError::TokenCreationError)?;
         Ok(web::Json(Token { token: token }))
     } else {
@@ -96,7 +105,11 @@ async fn validate_token(
     web::Json(Token { token }): web::Json<Token>,
     auth_mgr: web::Data<AuthManager>,
 ) -> actix_web::Result<String, MyError> {
-    if auth_mgr.validate_token::<i32>(token.clone()) {
+    let is_valid = web::block(move || auth_mgr.validate_token::<i32>(token.clone()))
+        .await
+        .map_err(|_| MyError::InternalServerError)?;
+
+    if is_valid {
         Ok("true".to_string())
     } else {
         Err(MyError::TokenValidationError)
@@ -118,16 +131,21 @@ async fn change_password(
     auth_mgr: web::Data<AuthManager>,
     db: web::Data<DbPool>,
 ) -> actix_web::Result<web::Json<UserData>, MyError> {
-    let user_id = auth_mgr
-        .extract_claim::<i32>(token)
+    let user_id = web::block(move || auth_mgr.extract_claim::<i32>(token))
+        .await
+        .map_err(|_| MyError::InternalServerError)?
         .map_err(|_| MyError::TokenValidationError)?;
 
     let mut conn = db.get().map_err(|_| MyError::InternalServerError)?;
-    let _ = get_user_by_user_id(conn, user_id).map_err(|_| MyError::UserDoesNotExists)?;
+    let _ = web::block(move || get_user_by_user_id(conn, user_id))
+        .await
+        .map_err(|_| MyError::InternalServerError)?
+        .map_err(|_| MyError::UserDoesNotExists)?;
 
     conn = db.get().map_err(|_| MyError::InternalServerError)?;
-
-    let user = update_user_password(conn, user_id, new_password)
+    let user = web::block(move || update_user_password(conn, user_id, new_password))
+        .await
+        .map_err(|_| MyError::InternalServerError)?
         .map_err(|err| MyError::DieselError(err))?;
 
     Ok(web::Json(UserData::from_user(user)))
