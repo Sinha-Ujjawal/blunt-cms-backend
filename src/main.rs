0b5af3ee7e761bb8e116e92e4ede4b3b7361f994
Sinha-Ujjawal <https://github.com/Sinha-Ujjawal>
2022-06-19 11:53:12 +0530

    Worker allocation- 50% server 35% db remaining auth_mgr

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use actix::{Addr, SyncArbiter};
 use actix_cors::Cors;
 use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 use dotenv::dotenv;
+use std::cmp::max;
 
 mod argon2_password_hasher;
 mod auth;
@@ -32,6 +33,7 @@ fn cors(cors_allow_all: u8) -> Cors {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // App configuration
     dotenv().ok();
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
@@ -48,19 +50,28 @@ async fn main() -> std::io::Result<()> {
         config.redis_server_get_connection_timeout,
     );
 
+    // Worker allocation
     let num_of_cpus = num_cpus::get();
+    let server_workers = max(num_of_cpus >> 1, 1);
+    let db_actor_workers = max((((num_of_cpus - server_workers) as f32) * 0.7).floor() as usize, 1);
+    let auth_mgr_workers = max(num_of_cpus - server_workers - db_actor_workers, 1);
 
-    let db_actor_addr = SyncArbiter::start(num_of_cpus, move || db_actor.clone());
-    let auth_mgr_addr = SyncArbiter::start(num_of_cpus >> 1, move || auth_mgr.clone());
+    log::info!("Number of Logical Cores: {}", num_of_cpus);
+    log::info!("Worker Allocation:");
+    log::info!("Num Server Workers: {}", server_workers);
+    log::info!("Num DB Workers: {}", db_actor_workers);
+    log::info!("Num Auth Mgr Workers: {}", auth_mgr_workers);
+
+    // Spawning workers
+    let db_actor_addr = SyncArbiter::start(db_actor_workers, move || db_actor.clone());
+    let auth_mgr_addr = SyncArbiter::start(auth_mgr_workers, move || auth_mgr.clone());
 
     let app_state = AppState {
         db_actor_addr: db_actor_addr,
         auth_mgr_addr: auth_mgr_addr,
     };
 
-    log::info!("Number of Logical Cores: {}", num_of_cpus);
-    log::info!("Started server on: http://{}:{}", host, port);
-
+    log::info!("Starting server on: http://{}:{}", host, port);
     HttpServer::new(move || {
         let cors = cors(config.cors_allow_all);
         App::new()
@@ -71,6 +82,7 @@ async fn main() -> std::io::Result<()> {
             .configure(views::posts::config)
             .configure(views::swagger_ui::config)
     })
+    .workers(server_workers)
     .bind((host, port))?
     .run()
     .await?;
